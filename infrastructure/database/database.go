fef1ebdbcbea19a34f8c4d6e3e8a2660237e8c25
tammysensitive2312 <dinhtruong1234lhp@gmail.com>
2024-08-14 13:57:44 +0700

    Golang project part 2

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -1,1 +1,32 @@
-package infrastructures
+package infrastructure
+
+import (
+	"Backend_golang_project/infrastructure/config"
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+)
+
+type IInitDatabase interface {
+}
+
+func NewInitDatabase(config *config.Config) (*gorm.DB, error) {
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.DB.Username,
+		config.DB.Password,
+		config.DB.Host,
+		config.DB.Port,
+		config.DB.DatabaseName,
+	)
+	db := mysql.Open(url)
+	gormDB, err := gorm.Open(db, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Can not connect db with err %v"), err)
+		return nil, err
+	}
+	return gormDB, nil
+}
