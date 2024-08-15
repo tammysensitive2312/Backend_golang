a9403351c8a350511d447c72c5485f20f40f3c64
tammysensitive2312 <dinhtruong1234lhp@gmail.com>
2024-08-15 14:18:26 +0700

    golang project part 4 v0.1

---
+++ internal/pkg/custom_validator.go
@@ -0,0 +1,36 @@
+package validators
+
+import (
+	"github.com/go-playground/validator/v10"
+	"regexp"
+	"time"
+)
+
+func PasswordValidator(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+	hasUppercase, _ := regexp.MatchString(`[A-Z]`, password)
+	hasLowercase, _ := regexp.MatchString(`[a-z]`, password)
+	hasNumber, _ := regexp.MatchString(`\d`, password)
+	return hasUppercase && hasLowercase && hasNumber
+}
+
+func CategoryValidator(fl validator.FieldLevel) bool {
+	category := fl.Field().String()
+	const (
+		CLIENT      = "client"
+		NonBillable = "non-billable"
+		SYSTEM      = "system"
+	)
+	validCategories := []string{CLIENT, NonBillable, SYSTEM}
+	for _, validCategory := range validCategories {
+		if category == validCategory {
+			return true
+		}
+	}
+	return false
+}
+
+func ProjectStartDateValidator(fl validator.FieldLevel) bool {
+	startDate := fl.Field().Interface().(time.Time)
+	return startDate.After(time.Now())
+}
--- internal/pkg/paginate.go
+++ internal/pkg/paginate.go
@@ -1,4 +1,4 @@
-package repositories
+package pkg
 
 import "Backend_golang_project/internal/domain/entities"
 
